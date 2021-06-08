d877412de070bd058de39351643de28ee3d73344
zhf_sy <zhf_sy@163.com>
2021-06-08 14:40:24 +0800

    OK

---
+++ env.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# sever env
+SERVER_CONF_FILE='/etc/wireguard/wg0.conf'
+SERVER_CONNECT_INFO='47.52.190.224:51801'
+WG_IF='wg0'
+IP_PREFIX='172.30.5'
+IP_NETMASK='24'
+SERVER_PUBKEY='4hgy39g5jUKU/KPzy28lQnIWEiV5tnIrnWfgbh1JcEU='
+SERVER_PRE_SHARED_KEY='2AbQpQnokHG5ta/vkwNolnKeb4MC40UzfOhMXBuY/uc='
+
+
+# user env
+USER_DNSs='192.168.11.3,192.168.11.4'
+USER_ALOWED_IPs='172.30.0.0/24,0.0.0.0/0'
+
+
+
+
