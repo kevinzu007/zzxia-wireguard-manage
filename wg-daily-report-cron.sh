b5fdee2aaca95b899008c47d9d66ad20ba5f47fd
zhf_sy <zhf_sy@163.com>
2021-07-18 15:59:14 +0800

    优化

--- wg-daily-report-cron.sh
+++ wg-daily-report-cron.sh
@@ -28,8 +28,12 @@ TIME=${TIME:-`date +%Y-%m-%dT%H:%M:%S`}
 TIME_START=${TIME}
 DATE_TIME=`date -d "${TIME}" +%Y%m%dt%H%M%S`
 CURRENT_DATE=`date -d "${TIME}" +%Y-%m-%d`
+#
+TODAY_WG_REPORT_FILE="/tmp/wg-report-today---${CURRENT_DATE}.md"
+FORMAT_TABLE_SH="${SH_PATH}/format_table.sh"
 
 
+echo '|日期|姓名|总流量MiB|IN流量MiB|OUT流量MiB|用户IP|用户公钥|远程IP|'  > ${TODAY_WG_REPORT_FILE}
 #
 while read LINE
 do
@@ -54,8 +58,13 @@ do
     # 是否有握手信息
     if [ ${USER_LATEST_HAND} -ne 0 ]; then
         # 有握手信息
+        # 写入总报表
         echo "| ${CURRENT_DATE} | ${USER_XINGMING} | ${USER_NET_TOTAL_MiB} | ${USER_NET_IN_MiB} | ${USER_NET_OUT_MiB} | ${USER_IP} | ${USER_PEER} | ${USER_ENDPOINT_IP} | " >> ${WG_REPORT_FILE}
+        # 今日报表
+        echo "| ${CURRENT_DATE} | ${USER_XINGMING} | ${USER_NET_TOTAL_MiB} | ${USER_NET_IN_MiB} | ${USER_NET_OUT_MiB} | ${USER_IP} | ${USER_PEER} | ${USER_ENDPOINT_IP} | " >> ${TODAY_WG_REPORT_FILE}
     fi
 done < ${WG_STATUS_CONLLECT_FILE}
+#
+${FORMAT_TABLE_SH}  --delimeter '|'  --title '|姓名|最后握手时间|总流量MiB|IN流量MiB|OUT流量MiB|用户IP|远程IP|'  --file ${TODAY_WG_REPORT_FILE}
 
 
