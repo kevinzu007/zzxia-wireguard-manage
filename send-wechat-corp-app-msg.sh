78ea851aae559a3346fba687e73ce6f0847aeb2a
zhf_sy <zhf_sy@163.com>
2026-08-21 18:02:56 +0800

    fix(notification): 优化企微/飞书消息排版，消除分割线多余空行并统一标题格式

--- send-wechat-corp-app-msg.sh
+++ send-wechat-corp-app-msg.sh
@@ -115,7 +115,7 @@ F_SEND_MSG()
     local url="https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=${token}"
 
     # 使用 %b 而不是 %s，这样就能自动将输入字符串里的 \n 转换成真正的换行符
-    printf -v content '# %s\n\n---\n\n%b\n\n---\n\n发自: **%s**\n时间: %s' \
+    printf -v content '### %s\n---\n%b\n---\n发自: **%s**\n时间: %s' \
         "${title}" "${message}" "${HOSTNAME}" "${DATETIME}"
 
     if command -v jq > /dev/null 2>&1; then
